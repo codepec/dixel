529c68fda8eac5ddc28474aa0c05477b1de67afc
codepec <codepec.de@gmail.com>
2024-05-30 14:49:52 +0200

    update

--- sw.js
+++ sw.js
@@ -4,44 +4,52 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-const CACHE = "/";
+const CACHE = "my-site-cache-v1";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "/offline.html";
+const offlineFallbackPage = "/pages/offline.html";
 
+// Enable navigation preload if supported
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+// Listen for the 'message' event to allow skipping waiting
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
 
-self.addEventListener("install", async (event) => {
+// Install event to cache the offline page
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE).then((cache) => {
+      return cache.add(offlineFallbackPage);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
+// Fetch event to serve the offline page for navigation requests
 self.addEventListener("fetch", (event) => {
   if (event.request.mode === "navigate") {
     event.respondWith(
       (async () => {
         try {
-          const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
+          // Try to use the preloaded response, if it's there
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
           }
 
-          const networkResp = await fetch(event.request);
-          return networkResp;
+          // Try the network first
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
         } catch (error) {
+          // Network request failed, serve offline page from cache
           const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
+          const cachedResponse = await cache.match(offlineFallbackPage);
+          return cachedResponse;
         }
       })()
     );
