abb21e4c60ae7762a9cdec848466c23367d93308
codepec <codepec.de@gmail.com>
2024-06-05 08:21:15 +0200

    update sw

--- sw.js
+++ sw.js
@@ -4,10 +4,26 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-const CACHE = "my-site-cache-v1";
+const CACHE = "my-site-cache-v3";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// Offline fallback page and other resources to cache
 const offlineFallbackPage = "./pages/offline.html";
+const assetsToCache = [
+  offlineFallbackPage,
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./img/icon_ki.jpg",
+  "./js/darkmode.js",
+  "./js/main.js",
+  "./js/script.js",
+  "./js/settings_lang_index",
+  "./js/settings_lang_instructions",
+  "./pages/offline.html",
+  "./pages/instructions.html",
+  "./pages/instructions.css",
+  "./favicon.ico",
+];
 
 // Enable navigation preload if supported
 if (workbox.navigationPreload.isSupported()) {
@@ -21,11 +37,11 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// Install event to cache the offline page
+// Install event to cache the offline page and other assets
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE).then((cache) => {
-      return cache.add(offlineFallbackPage);
+      return cache.addAll(assetsToCache);
     })
   );
 });
